0ade6f515d6700c2d4037bb32c66ea65611f0bc1
Gommez <alexandre.gommez@edu.ece.fr>
2021-10-16 16:26:24 +0200

    Loading data

--- src/db_loading.hpp
+++ src/db_loading.hpp
@@ -1,44 +1,59 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <string.h>
-#include <stdio.h>
+int loading(string file_name, Table table)
+{
+   string line;
+   string delimiter = "|";
+   ifstream file(file_name);
+   if (file.is_open())
+   {
+      
+      while (!file.eof())
+      {
+         getline(file, line);
+         cout<<line<<endl;
+      }
+      file.close();
+   }
+   return 0;
+}
 
-#include "structure.hpp"
-
-using namespace std;
-
-int nb_l_fichier(string file_name){
+/* 
+int nb_l_fichier(string file_name)
+{
    int numLines = 0;
    std::string line;
    ifstream file(file_name);
-   if(file.is_open()){
-      while(!file.eof()){
-         getline(file,line);
-         if(line.length()>0)  numLines++;
+   if (file.is_open())
+   {
+      while (!file.eof())
+      {
+         getline(file, line);
+         if (line.length() > 0)
+            numLines++;
       }
       file.close();
    }
    return numLines;
 };
 
-void row_nation_loading(nation_tuple *nation_table){
-   std::string delimiter = "|";
+void row_nation_loading(nation_tuple *nation_table)
+{
    std::string file_name = "..\\Scale 1\\nation.tbl";
    std::string line;
    int pos = 0;
    ifstream file(file_name);
    nation_tuple *tuple;
    tuple = nation_table;
-   if(file.is_open()){
-      getline(file,line);
-      while(!file.eof()){
+   if (file.is_open())
+   {
+      getline(file, line);
+      while (!file.eof())
+      {
          pos = line.find(delimiter);
          (*tuple).N_NATIONKEY = std::stoi(line.substr(0, pos));
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).N_NAME = line.substr(0, pos) ;
+         (*tuple).N_NAME = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
@@ -46,17 +61,18 @@ void row_nation_loading(nation_tuple *nation_table){
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).N_COMMENT = line.substr(0, pos) ;
+         (*tuple).N_COMMENT = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
-         tuple ++;
-         getline(file,line);
+         tuple++;
+         getline(file, line);
       }
       file.close();
    }
 };
 
-void row_region_loading(region_tuple *region_table){
+void row_region_loading(region_tuple *region_table)
+{
    std::string delimiter = "|";
    std::string file_name = "..\\Scale 1\\region.tbl";
    std::string line;
@@ -64,29 +80,32 @@ void row_region_loading(region_tuple *region_table){
    ifstream file(file_name);
    region_tuple *tuple;
    tuple = region_table;
-   if(file.is_open()){
-      getline(file,line);
-      while(!file.eof()){
+   if (file.is_open())
+   {
+      getline(file, line);
+      while (!file.eof())
+      {
          pos = line.find(delimiter);
          (*tuple).R_REGIONKEY = std::stoi(line.substr(0, pos));
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).R_NAME = line.substr(0, pos) ;
+         (*tuple).R_NAME = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).R_COMMENT = line.substr(0, pos) ;
+         (*tuple).R_COMMENT = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
-         tuple ++;
-         getline(file,line);
+         tuple++;
+         getline(file, line);
       }
       file.close();
    }
 };
 
-void row_customer_loading(customer_tuple *customer_table){
+void row_customer_loading(customer_tuple *customer_table)
+{
    std::string delimiter = "|";
    std::string file_name = "..\\Scale 1\\customer.tbl";
    std::string line;
@@ -94,19 +113,21 @@ void row_customer_loading(customer_tuple *customer_table){
    ifstream file(file_name);
    customer_tuple *tuple;
    tuple = customer_table;
-   if(file.is_open()){
-      getline(file,line);
-      while(!file.eof()){
+   if (file.is_open())
+   {
+      getline(file, line);
+      while (!file.eof())
+      {
          pos = line.find(delimiter);
          (*tuple).C_CUSTKEY = std::stoi(line.substr(0, pos));
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).C_NAME = line.substr(0, pos) ;
+         (*tuple).C_NAME = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).C_ADDRESS = line.substr(0, pos) ;
+         (*tuple).C_ADDRESS = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
@@ -114,7 +135,7 @@ void row_customer_loading(customer_tuple *customer_table){
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).C_PHONE = line.substr(0, pos) ;
+         (*tuple).C_PHONE = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
@@ -122,22 +143,23 @@ void row_customer_loading(customer_tuple *customer_table){
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).C_MKTSEGMENT = line.substr(0, pos) ;
+         (*tuple).C_MKTSEGMENT = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
          pos = line.find(delimiter);
-         (*tuple).C_COMMENT = line.substr(0, pos) ;
+         (*tuple).C_COMMENT = line.substr(0, pos);
          line.erase(0, pos + delimiter.length());
 
-         tuple ++;
-         getline(file,line);
+         tuple++;
+         getline(file, line);
       }
       file.close();
    }
 };
 
-void test(customer_tuple *customer_table){
+void test(customer_tuple *customer_table)
+{
    std::cout << sizeof(customer_table) << std::endl;
    std::cout << sizeof(*customer_table) << std::endl;
    std::cout << sizeof(customer_tuple) << std::endl;
-};
+}; */
