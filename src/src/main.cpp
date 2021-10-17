1c3f231cdd93fedea87a24f5ee27c43b1e65b840
BENNANI MEHDY p1711302 <mehdy.bennani@etu.univ-lyon1.fr>
2021-10-17 16:46:37 +0200

    Makefile et fonction sommme et selection

---
+++ src/src/main.cpp
@@ -0,0 +1,31 @@
+#include "db_loading.hpp"
+using namespace std;
+int main() {
+  vector<string> cols_names = { 
+    "P_PARTKEY", "P_NAME", "P_MFGR", "P_BRAND", "P_TYPE",
+    "P_SIZE", "P_CONTAINER", "P_RETAILPRICE", "P_COMMENT"
+  };
+  vector<string> types = { 
+    "Integer","55", "25", "10", "25", "Integer", "10", "Float", "23"
+  };
+  Table part(cols_names, types);
+
+  vector<string> values = { 
+    "5","test1", "test2", "test3", "test4", "10", "test5", "5.5", "test6"
+  };
+  loading("..\\data\\nation.tbl",part);
+  part.add_row(values);
+  part.add_row(values);
+  part.add_row(values);
+  for (int i = 0; i < part.m_cols_names.size(); i++)
+    cout << part.m_cols_names[i] << endl;
+  for (int i = 0; i < part.m_cols.size(); i++)
+    part.m_cols[i]->print_values();
+  vector<string> quer = {"P_PARTKEY","P_RETAILPRICE","P_COMMENT"};
+  Table *subpart = part.selection(quer);
+  subpart->sum("P_PARTKEY");
+  vector<string> *tmp=new vector<string>;
+  *tmp=quer;
+  cout<<(*tmp)[0]<<endl;
+  return 0;
+}
