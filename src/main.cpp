4c7eaa0d44f73d362da764e7aceee6bd90e1654d
Gommez <alexandre.gommez@edu.ece.fr>
2021-10-18 15:51:31 +0200

    Test de distinct

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include "db_loading.hpp"
 int main()
 {
-    /*
+    
     time_t begin, end; // time_t is a datatype to store time values.
     time(&begin);
 
-    //1 - table part ************************************************************
+     //1 - table part ************************************************************
     vector<string> cols_names = {
         "P_PARTKEY", "P_NAME", "P_MFGR", "P_BRAND",
         "P_TYPE", "P_SIZE", "P_CONTAINER", "P_RETAILPRICE", "P_COMMENT"};
     vector<string> types = {
         "Integer", "55", "25", "10", "25", "Integer", "10", "Float", "23"};
-    Table part_t(cols_names, types);
+    /*Table part_t(cols_names, types);
     loading("..\\data\\part.tbl", &part_t);
 
     //2 - table Supplier *******************************************************
@@ -37,18 +37,18 @@ int main()
     types = {
         "Integer", "25", "40", "Integer", "15", "Float", "10", "117"};
     Table customer_t(cols_names, types);
-    loading("..\\data\\customer.tbl", &customer_t);
+    loading("..\\data\\customer.tbl", &customer_t); */
 
-    //5 - table orders******************************************************************
+/*     //5 - table orders******************************************************************
     cols_names = {
         "O_ORDERKEY", "O_CUSTKEY", "O_ORDERSTATUS", "O_TOTALPRICE",
         "O_ORDERDATE", "O_ORDERPRIORITY", "O_CLERK", "O_SHIPPPRIORITY", "O_COMMENT"};
     types = {
         "Integer", "Integer", "1", "Float", "10", "15", "15", "Integer", "79"};
     Table orders_t(cols_names, types);
-    loading("..\\data\\orders.tbl", &orders_t);
+    loading("..\\data\\orders.tbl", &orders_t); */
 
-    //6 - table lineitem******************************************************************
+/*     //6 - table lineitem******************************************************************
     cols_names = {
         "L_ORDERKEY", "L_PARTKEY", "L_SUPPKEY", "L_LINENUMBER",
         "L_QUANTITY", "L_EXTENDEDPRICE", "L_DISCOUNT", "L_TAX", "L_RETURNFLAG",
@@ -58,15 +58,15 @@ int main()
         "Integer", "Integer", "Integer", "Integer", "Float", "Float", "Float", "Float",
         "1", "1", "10", "10", "10", "25", "10", "44"};
     Table lineitem_t(cols_names, types);
-    loading("..\\data\\lineitem.tbl", &lineitem_t);
+    loading("..\\data\\lineitem.tbl", &lineitem_t); */
 
-    //7 - table Nation *********************************************************
+/*     //7 - table Nation *********************************************************
     cols_names = {
         "N_NATIONKEY", "N_NAME", "N_REGIONKEY", "N_COMMENT"};
     types = {
         "Integer", "25", "Integer", "152"};
     Table nation_t(cols_names, types);
-    -loading("..\\data\\nation.tbl", &nation_t);
+    -loading("..\\data\\nation.tbl", &nation_t); */
 
     //8 - table region ***********************************************************
     cols_names = {
@@ -75,10 +75,11 @@ int main()
         "Integer", "25", "152"};
     Table region_t(cols_names, types);
     loading("..\\data\\region.tbl", &region_t);
+    loading("..\\data\\region.tbl", &region_t);
 
     time(&end);
 
-    double difference = difftime(end, begin);
+/*     double difference = difftime(end, begin);
     printf("time taken for function() %.2lf seconds.\n", difference);
 
     cout << sum("R_REGIONKEY", region_t) << endl;
@@ -89,11 +90,8 @@ int main()
     result.print();
 
     cout << sum("R_REGIONKEY", result) << endl;
-    cout << avg("R_REGIONKEY", result) << endl;
-    /*vector<string> *tmp = new vector<string>;
-  *tmp = quer;
-  cout << (*tmp)[0] << endl; */
-    vector<string> cols_names = {
+    cout << avg("R_REGIONKEY", result) << endl; */
+/*     vector<string> cols_names = {
         "P_PARTKEY", "P_NAME", "P_MFGR", "P_BRAND", "P_TYPE",
         "P_SIZE", "P_CONTAINER", "P_RETAILPRICE", "P_COMMENT"};
     vector<string> types = {
@@ -108,14 +106,11 @@ int main()
     part.add_row(values);
     part.add_row(values2);
     part.add_row(values);
-    part.add_row(values);
-    for (int i = 0; i < part.m_cols.size(); i++){part.m_cols[i]->print_values();}
+    part.add_row(values); */
+    region_t.print();
     cout<<"On applique le distinct"<<endl;
-    distinct(part);
+    distinct(region_t);
     cout<<"Apres le distinct"<<endl;
-    for (int i = 0; i < part.m_cols.size(); i++)
-    {
-        part.m_cols[i]->print_values();
-    }
+    region_t.print();
     return 0;
 }
