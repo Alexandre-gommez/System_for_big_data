308593f44693209b00d47f4ac27aa68eaf25647f
LSQ <h.lsq@orange.fr>
2021-10-20 12:23:06 +0200

    avancement stockage row

--- src/structure.hpp
+++ src/structure.hpp
@@ -5,6 +5,79 @@
 
 using namespace std;
 
+// 1. part
+typedef struct{
+    int P_PARTKEY;
+    string P_NAME;
+    string P_MFGR;
+    string P_BRAND;
+    string P_TYPE;
+    int P_SIZE;
+    string P_CONTAINER;
+    float P_RETAILPRICE;
+    string P_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(P_PARTKEY));
+        temp.push_back(P_NAME);
+        temp.push_back(P_MFGR);
+        temp.push_back(P_BRAND);
+        temp.push_back(P_TYPE);
+        temp.push_back(to_string(P_SIZE));
+        temp.push_back(P_CONTAINER);
+        temp.push_back(to_string(P_RETAILPRICE));
+        temp.push_back(P_COMMENT);
+        return temp;
+    }
+}tuple_part;
+
+//2. supplier
+typedef struct{
+    int S_SUPPKEY;
+    string S_NAME;
+    string S_ADDRESS;
+    int S_NATIONKEY;
+    string S_PHONE;
+    float S_ACCTBAL;
+    string S_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(S_SUPPKEY));
+        temp.push_back(S_NAME);
+        temp.push_back(S_ADDRESS);
+        temp.push_back(to_string(S_NATIONKEY));
+        temp.push_back(S_PHONE);
+        temp.push_back(to_string(S_ACCTBAL));
+        temp.push_back(S_COMMENT);
+        return temp;
+    }
+}tuple_supplier;
+
+//3. PARTSUPP
+typedef struct{
+    int PS_PARTKEY;
+    int PS_SUPPKEY;
+    int PS_AVAILQTY;
+    float PS_SUPPLYCOST;
+    string PS_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(PS_PARTKEY));
+        temp.push_back(to_string(PS_SUPPKEY));
+        temp.push_back(to_string(PS_AVAILQTY));
+        temp.push_back(to_string(PS_SUPPLYCOST));
+        temp.push_back(PS_COMMENT);
+        return temp;
+    }
+}tuple_partsupp;
+
+//4. CUSTOMER
 typedef struct{
     int C_CUSTKEY;
     string C_NAME;
@@ -14,8 +87,93 @@ typedef struct{
     float C_ACCTBAL;
     string C_MKTSEGMENT;
     string C_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(C_CUSTKEY));
+        temp.push_back(C_NAME);
+        temp.push_back(C_ADDRESS);
+        temp.push_back(to_string(C_NATIONKEY));
+        temp.push_back(C_PHONE);
+        temp.push_back(to_string(C_ACCTBAL));
+        temp.push_back(C_MKTSEGMENT);
+        temp.push_back(C_COMMENT);
+        return temp;
+    }
 }tuple_customer;
 
+//5. ORDERS
+typedef struct{
+    int O_ORDERKEY;
+    int O_CUSTKEY;
+    string O_ORDERSTATUS;
+    float O_TOTALPRICE;
+    string O_ORDERDATE;
+    string O_ORDERPRIORITY;
+    string O_CLERK;
+    int O_SHIPPRIORITY;
+    string O_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(O_ORDERKEY));
+        temp.push_back(to_string(O_CUSTKEY));
+        temp.push_back(O_ORDERSTATUS);
+        temp.push_back(to_string(O_TOTALPRICE));
+        temp.push_back(O_ORDERDATE);
+        temp.push_back(O_ORDERPRIORITY);
+        temp.push_back(O_CLERK);
+        temp.push_back(to_string(O_SHIPPRIORITY));
+        temp.push_back(O_COMMENT);
+        return temp;
+    }
+}tuple_orders;
+
+//6. LINEITEM
+typedef struct{
+    int L_ORDERKEY;
+    int L_PARTKEY;
+    int L_SUPPKEY;
+    int L_LINENUMBER;
+    float L_QUANTITY;
+    float L_EXTENDEDPRICE;
+    float L_DISCOUNT;
+    float L_TAX;
+    string L_RETURNFLAG;
+    string L_LINESTATUS;
+    string L_SHIPDATE;
+    string L_COMMITDATE;
+    string L_RECEIPTDATE;
+    string L_SHIPINSTRUCT;
+    string L_SHIPMODE;
+    string L_COMMENT;
+
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(L_ORDERKEY));
+        temp.push_back(to_string(L_PARTKEY));
+        temp.push_back(to_string(L_SUPPKEY));
+        temp.push_back(to_string(L_LINENUMBER));
+        temp.push_back(to_string(L_QUANTITY));
+        temp.push_back(to_string(L_EXTENDEDPRICE));
+        temp.push_back(to_string(L_DISCOUNT));
+        temp.push_back(to_string(L_TAX));
+        temp.push_back(L_RETURNFLAG);
+        temp.push_back(L_LINESTATUS);
+        temp.push_back(L_SHIPDATE);
+        temp.push_back(L_COMMITDATE);
+        temp.push_back(L_RECEIPTDATE);
+        temp.push_back(L_SHIPINSTRUCT);
+        temp.push_back(L_SHIPMODE);
+        temp.push_back(L_COMMENT);
+        return temp;
+    }
+}tuple_lineitem;
+
+//7. NATION
 typedef struct{
     int N_NATIONKEY;
     string N_NAME;
@@ -29,20 +187,22 @@ typedef struct{
         temp.push_back(N_NAME);
         temp.push_back(to_string(N_REGIONKEY));
         temp.push_back(N_COMMENT);
-
         return temp;
     }
 }tuple_nation;
 
+//8. REGION
 typedef struct{
     int R_REGIONKEY;
     string R_NAME;
     string R_COMMENT;
-}tuple_region;
 
-//typedef struct{
-//    std::vector<int> N_NATION;
-//    std::vector<std::string> N_NAME;
-//    std::vector<int> N_REGIONKEY;
-//    std::vector<std::string> N_COMMENT;
-//}nation_col;
+    vector<string> vector_()
+    {
+        vector<string> temp;
+        temp.push_back(to_string(R_REGIONKEY));
+        temp.push_back(R_NAME);
+        temp.push_back(R_COMMENT);
+        return temp;
+    }
+}tuple_region;
--- src/table_row.hpp
+++ src/table_row.hpp
@@ -2,7 +2,351 @@
 #include "structure.hpp"
 
 using namespace std;
+//1. PART**************************************************************
+class Table_row_p
+{
+public:
+    vector<string> m_cols_names;
+    vector<string> m_cols_types;
+    vector<tuple_part> m_tuples;
+    int n_row;
 
+    Table_row_p() {}
+
+    Table_row_p(vector<string> cols_names, vector<string> cols_types, string file_name)
+    {
+        n_row = 0;
+        m_cols_names = cols_names;
+        m_cols_types = cols_types;
+        ifstream file(file_name);
+        string line;
+        string delimiter = "|";
+        tuple_part tuple;
+        int pos=0;
+        if (file.is_open())
+        {
+            getline(file, line);
+            while (!file.eof())
+            {
+                pos = line.find(delimiter);
+                tuple.P_PARTKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_NAME = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_MFGR = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_BRAND = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_TYPE = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_SIZE = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+            
+                pos = line.find(delimiter);
+                tuple.P_CONTAINER = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_RETAILPRICE = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.P_COMMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                m_tuples.push_back(tuple);
+                n_row ++;
+                getline(file,line);
+            }
+            file.close();
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < n_row; i++)
+        {
+            for (const string word : m_tuples[i].vector_())
+            {
+                cout << word << " - ";
+            }
+            cout << endl;
+        }
+    }
+
+    void concat(Table_row_p tab2)
+    {
+        for (int i = 0; i < tab2.n_row; i++)
+        {
+            n_row ++;
+            m_tuples.push_back(tab2.m_tuples[i]);
+        }
+    }
+};
+
+//2. SUPPLIER**************************************************************
+class Table_row_s
+{
+public:
+    vector<string> m_cols_names;
+    vector<string> m_cols_types;
+    vector<tuple_supplier> m_tuples;
+    int n_row;
+
+    Table_row_s() {}
+
+    Table_row_s(vector<string> cols_names, vector<string> cols_types, string file_name)
+    {
+        n_row = 0;
+        m_cols_names = cols_names;
+        m_cols_types = cols_types;
+        ifstream file(file_name);
+        string line;
+        string delimiter = "|";
+        tuple_supplier tuple;
+        int pos=0;
+        if (file.is_open())
+        {
+            getline(file, line);
+            while (!file.eof())
+            {
+                pos = line.find(delimiter);
+                tuple.S_SUPPKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_NAME = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_ADDRESS = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_NATIONKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_PHONE = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_ACCTBAL = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.S_COMMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                m_tuples.push_back(tuple);
+                n_row ++;
+                getline(file,line);
+            }
+            file.close();
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < n_row; i++)
+        {
+            for (const string word : m_tuples[i].vector_())
+            {
+                cout << word << " - ";
+            }
+            cout << endl;
+        }
+    }
+
+    void concat(Table_row_s tab2)
+    {
+        for (int i = 0; i < tab2.n_row; i++)
+        {
+            n_row ++;
+            m_tuples.push_back(tab2.m_tuples[i]);
+        }
+    }
+};
+
+//3. PARTSUPP**************************************************************
+class Table_row_ps
+{
+public:
+    vector<string> m_cols_names;
+    vector<string> m_cols_types;
+    vector<tuple_partsupp> m_tuples;
+    int n_row;
+
+    Table_row_ps() {}
+
+    Table_row_ps(vector<string> cols_names, vector<string> cols_types, string file_name)
+    {
+        n_row = 0;
+        m_cols_names = cols_names;
+        m_cols_types = cols_types;
+        ifstream file(file_name);
+        string line;
+        string delimiter = "|";
+        tuple_partsupp tuple;
+        int pos=0;
+        if (file.is_open())
+        {
+            getline(file, line);
+            while (!file.eof())
+            {
+                pos = line.find(delimiter);
+                tuple.PS_PARTKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.PS_SUPPKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.PS_AVAILQTY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.PS_SUPPLYCOST = std::stof(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.PS_COMMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                m_tuples.push_back(tuple);
+                n_row ++;
+                getline(file,line);
+            }
+            file.close();
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < n_row; i++)
+        {
+            for (const string word : m_tuples[i].vector_())
+            {
+                cout << word << " - ";
+            }
+            cout << endl;
+        }
+    }
+
+    void concat(Table_row_ps tab2)
+    {
+        for (int i = 0; i < tab2.n_row; i++)
+        {
+            n_row ++;
+            m_tuples.push_back(tab2.m_tuples[i]);
+        }
+    }
+};
+
+//4. CUSTOMER**************************************************************
+class Table_row_c
+{
+public:
+    vector<string> m_cols_names;
+    vector<string> m_cols_types;
+    vector<tuple_customer> m_tuples;
+    int n_row;
+
+    Table_row_c() {}
+
+    Table_row_c(vector<string> cols_names, vector<string> cols_types, string file_name)
+    {
+        n_row = 0;
+        m_cols_names = cols_names;
+        m_cols_types = cols_types;
+        ifstream file(file_name);
+        string line;
+        string delimiter = "|";
+        tuple_customer tuple;
+        int pos=0;
+        if (file.is_open())
+        {
+            getline(file, line);
+            while (!file.eof())
+            {
+                pos = line.find(delimiter);
+                tuple.C_CUSTKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_NAME = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_ADDRESS = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_NATIONKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_PHONE = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_ACCTBAL = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_MKTSEGMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.C_COMMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                m_tuples.push_back(tuple);
+                n_row ++;
+                getline(file,line);
+            }
+            file.close();
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < n_row; i++)
+        {
+            for (const string word : m_tuples[i].vector_())
+            {
+                cout << word << " - ";
+            }
+            cout << endl;
+        }
+    }
+
+    void concat(Table_row_c tab2)
+    {
+        for (int i = 0; i < tab2.n_row; i++)
+        {
+            n_row ++;
+            m_tuples.push_back(tab2.m_tuples[i]);
+        }
+    }
+};
+
+//7. NATION**************************************************************
 class Table_row_n
 {
 public:
@@ -66,6 +410,75 @@ public:
     }
 
     void concat(Table_row_n tab2)
+    {
+        for (int i = 0; i < tab2.n_row; i++)
+        {
+            n_row ++;
+            m_tuples.push_back(tab2.m_tuples[i]);
+        }
+    }
+};
+
+//8. REGION**************************************************************
+class Table_row_r
+{
+public:
+    vector<string> m_cols_names;
+    vector<string> m_cols_types;
+    vector<tuple_region> m_tuples;
+    int n_row;
+
+    Table_row_r() {}
+
+    Table_row_r(vector<string> cols_names, vector<string> cols_types, string file_name)
+    {
+        n_row = 0;
+        m_cols_names = cols_names;
+        m_cols_types = cols_types;
+        ifstream file(file_name);
+        string line;
+        string delimiter = "|";
+        tuple_region tuple;
+        int pos=0;
+        if (file.is_open())
+        {
+            getline(file, line);
+            while (!file.eof())
+            {
+                pos = line.find(delimiter);
+                tuple.R_REGIONKEY = std::stoi(line.substr(0, pos));
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.R_NAME = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                pos = line.find(delimiter);
+                tuple.R_COMMENT = line.substr(0, pos) ;
+                line.erase(0, pos + delimiter.length());
+
+                m_tuples.push_back(tuple);
+                n_row ++;
+                getline(file,line);
+            }
+            file.close();
+        }
+    }
+
+
+    void print()
+    {
+        for (int i = 0; i < n_row; i++)
+        {
+            for (const string word : m_tuples[i].vector_())
+            {
+                cout << word << " - ";
+            }
+            cout << endl;
+        }
+    }
+
+    void concat(Table_row_r tab2)
     {
         for (int i = 0; i < tab2.n_row; i++)
         {
